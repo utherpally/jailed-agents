4b0a8f597a8085430812b4e402c1b6c73ee966ac
Uther Pally <utherpally@protonmail.com>
2026-02-04 16:21:23 +0700

    init

---
+++ flake.nix
@@ -0,0 +1,88 @@
+{
+  description = "Run LLM agent in sandbox environment";
+  inputs = {
+    jail-nix.url = "sourcehut:~alexdavid/jail.nix";
+    llm-agents.url = "github:numtide/llm-agents.nix";
+  };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.numtide.com"
+    ];
+    extra-trusted-public-keys = [
+      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+    ];
+  };
+
+  outputs =
+    {
+      self,
+      jail-nix,
+      llm-agents,
+    }:
+    {
+      lib = {
+        mkJailedClaudeCode =
+          {
+            pkgs,
+            extraPkgs ? [ ],
+          }:
+          self.lib.mkJailedAgent {
+            inherit pkgs;
+            name = "jailed-claude";
+            agent = agents: agents.claude-code;
+            permissions =
+              combinators: with combinators; [
+                (readwrite (noescape "~/.claude"))
+                (readwrite (noescape "~/.claude.json"))
+                (add-pkg-deps extraPkgs)
+              ];
+          };
+        mkJailedAgent =
+          {
+            name,
+            agent,
+            pkgs,
+            permissions ? (combinators: [ ]),
+            defaultOption ? true,
+            defaultPkg ? true,
+          }:
+          let
+            jail = jail-nix.lib.init pkgs;
+            combinators = jail.combinators;
+            agents = llm-agents.packages.${pkgs.stdenv.hostPlatform.system};
+            jail-agent = if (builtins.typeOf agent) == "lambda" then agent agents else agent;
+
+            defaultOptions = with combinators; [
+              network
+              time-zone
+              no-new-session
+              mount-cwd
+            ];
+            defaultPkgs = with pkgs; [
+              bashInteractive
+              curl
+              wget
+              jq
+              git
+              which
+              ripgrep
+              gnugrep
+              gawkInteractive
+              ps
+              findutils
+              gzip
+              unzip
+              gnutar
+              diffutils
+            ];
+            optionals = pkgs.lib.optionals;
+          in
+          jail name jail-agent (
+            (optionals defaultOption defaultOptions)
+            ++ (optionals defaultPkg [ (combinators.add-pkg-deps defaultPkgs) ])
+            ++ (permissions combinators)
+          );
+      };
+    };
+}
